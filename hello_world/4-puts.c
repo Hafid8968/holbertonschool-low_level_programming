a454781865def9410db134eae3d2ba84fb191c64
Hafid8968 <8968@holbertonstudents.com>
2024-02-19 22:19:53 +0100

    Add C program using puts to print a multilingual puzzle

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -2,5 +2,5 @@
 
 /* This program prints "Programming is like building a multilingual puzzle" */
 int main(void) {
-    puts("\"Programming is like building a multilingual puzzle\"");
+    puts("\"Programming is like building a multilingual puzzle");
     return 0;
