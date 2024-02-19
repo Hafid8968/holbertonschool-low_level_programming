a226324fa8e0ab5dea5771d350419dc938be20a3
Hafid8968 <8968@holbertonstudents.com>
2024-02-19 22:58:41 +0100

    Add C program using puts to print a multilingual puzzle

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -1,7 +1,11 @@
 #include <stdio.h>
 
 /* This program prints "Programming is like building a multilingual puzzle" */
-int main(void) {
-    puts("\"Programming is like building a multilingual puzzle");
-    return 0;
+int main(void)
+{
+/* Fix: Open brace '{' following function declarations goes on the next line */
+         puts("\"Programming is like building a multilingual puzzle");
+/* Fix: No spaces at the start of a line */
+/* Fix: Parentheses are required on a return statement */
+        return (0);
 }
