4fa8b274f5b3d9b8e9b050fc1cb774c2d18acc0e
Hafid8968 <8968@holbertonstudents.com>
2024-02-19 22:11:48 +0100

    Add C program using puts to print a multilingual puzzle

---
+++ hello_world/4-puts.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+// This main function serves as the entry point of the program
+int main(void) {
+    // Using the puts function to display the desired message with included quotes
+    puts("\"Programming is like building a multilingual puzzle\"");
+
+    // The program concludes by returning the value 0
+    return 0;
+}
+
