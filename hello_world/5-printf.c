06a8a6cf1ac202c53615cc5eff6d1b2c10d578ca
Hafid8968 <8968@holbertonstudents.com>
2024-02-20 09:54:10 +0100

    Add C program using printf to create a piece of art

---
+++ hello_world/5-printf.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/* This program prints "with proper grammar, but the outcome is a piece of art," */
+int main(void)
+{
+/* Fix: Open brace '{' following function declarations goes on the next line */
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+/* Fix: No spaces at the start of a line */
+/* Fix: Parentheses are required on a return statement */
+	return (0);
+}
+
