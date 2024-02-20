62fae3f9aba0f7c9a4e8c480a831698645d20e17
Hafid8968 <8968@holbertonstudents.com>
2024-02-20 15:18:53 +0100

    Add completed source code to determine positivity of a random number

--- variables_if_else_while/0-positive_or_negative.c
+++ variables_if_else_while/0-positive_or_negative.c
@@ -1,16 +1,26 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
-/* more headers goes there */
 
-/* betty style doc for function main goes there */
+/**
+ * main - Entry point
+ *
+ * This program generates a random number and determines if it's positive,
+ * negative, or zero. The random number is generated each time the program is executed.
+ *
+ * Return: Always 0 (Success)
+ */
 int main(void)
 {
-	int n;
+    int n;
 
-	srand(time(0));
-	n = rand() - RAND_MAX / 2;
-    
+    // Seed the random number generator with the current time
+    srand(time(0));
+
+    // Generate a random number and assign it to the variable n
+    n = rand() - RAND_MAX / 2;
+
+    // Check if n is positive, negative, or zero, and print the result
     if (n > 0) {
         printf("%d is positive\n", n);
     } else if (n == 0) {
@@ -19,5 +29,6 @@ int main(void)
         printf("%d is negative\n", n);
     }
 
-	return (0);
+    return (0);
 }
+
