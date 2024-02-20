679b25610efd53a3224c135922cbf3b8d9acb19d
Hafid8968 <8968@holbertonstudents.com>
2024-02-20 13:37:14 +0100

    Add C program to print size of various types

--- hello_world/6-size.c
+++ hello_world/6-size.c
@@ -7,14 +7,15 @@
  *
  * Return: Always 0 (Success)
  */
+int main(void)
+{
+	/* Utilisation de sizeof pour obtenir la taille des types de données */
+	printf("Size of a char: %zu byte(s)\n", sizeof(char));
+	printf("Size of an int: %zu byte(s)\n", sizeof(int));
+	printf("Size of a long int: %zu byte(s)\n", sizeof(long int));
+	printf("Size of a long long int: %zu byte(s)\n", sizeof(long long int));
+	printf("Size of a float: %zu byte(s)\n", sizeof(float));
 
-int main(void) {
-    // Utilisation de sizeof pour obtenir la taille des types de données
-    printf("Size of a char: %zu byte(s)\n", sizeof(char));
-    printf("Size of an int: %zu byte(s)\n", sizeof(int));
-    printf("Size of a long int: %zu byte(s)\n", sizeof(long int));
-    printf("Size of a long long int: %zu byte(s)\n", sizeof(long long int));
-    printf("Size of a float: %zu byte(s)\n", sizeof(float));
-
-    return (0);
+	return (0);
 }
+
