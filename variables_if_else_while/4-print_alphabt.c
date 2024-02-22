3c131dd7a40cfc8ac2337945d52c95276e2f63c0
Hafid8968 <8968@holbertonstudents.com>
2024-02-22 20:43:33 +0100

    Add program to print modified alphabet using putchar

---
+++ variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main(void) {
+        char alphabet;
+
+        for (alphabet = 'a'; alphabet <= 'z'; alphabet++) {
+            /* Exclude the letters 'e' and 'i' from printing */
+            if (alphabet != 'e' && alphabet != 'i') {
+                putchar(alphabet);
+            }
+        }
+
+        /* Ajout d'un saut de ligne pour une meilleure lisibilité */
+        putchar('\n');
+
+        return 0;
+}
