a8cd660e6f8498226b953640f6174e1a352a9883
Hafid8968 <8968@holbertonstudents.com>
2024-02-25 18:29:57 +0100

    Add source code to print the last digit of a random number

--- variables_if_else_while/1-last_digit.c
+++ variables_if_else_while/1-last_digit.c
@@ -3,27 +3,29 @@
 /* Added the following header for printf */
 #include <stdio.h>
 
-/* betty style doc for function main goes there */
+/**
+ * main - more headers goes there
+ *
+ * Return: 0
+ */
 int main(void)
 {
-    int n;
-    int lastDigit;
+	int n;
+	int lastDigit;
 
-    srand(time(0));
-    n = rand() - RAND_MAX / 2;
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
 
-    /* Extract the last digit of the number */
-    lastDigit = n % 10;
+	lastDigit = n % 10;
 
-    /* Check if the last digit is less than 5 and not equal to 0 */
-    if (lastDigit > 5) {
+	if (lastDigit > 5) {
         printf("Last digit of %d is %d and is greater than 5\n", n, lastDigit);
-    } else if (lastDigit == 0) {
+	} else if (lastDigit == 0) {
         printf("Last digit of %d is %d and is 0\n", n, lastDigit);
-    } else {
+	} else {
         printf("Last digit of %d is %d and is less than 6 and not 0\n", n, lastDigit);
-    }
+	}
 
-    return (0);
+	return (0);
 }
 
