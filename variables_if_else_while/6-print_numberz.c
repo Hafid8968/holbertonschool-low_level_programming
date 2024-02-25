e42a7823b6c8026a9ec618f7e77b5403e3410f07
Hafid8968 <8968@holbertonstudents.com>
2024-02-25 20:52:13 +0100

    Add program to print single-digit numbers in base 10 using putchar

--- variables_if_else_while/6-print_numberz.c
+++ variables_if_else_while/6-print_numberz.c
@@ -1,15 +1,22 @@
 #include <stdio.h>
 
-int main(void) {
-    int numbers;
+/**
+ * main - Prints all single-digit numbers of base 10 starting from 0.
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int numbers;
 
-    for (numbers = 0; numbers <= 9; numbers++) {
-        putchar('0' + numbers);
-    }
+	for (numbers = 0; numbers <= 9; numbers++)
+	{
+		putchar('0' + numbers);
+	}
 
-    /* Ajout d'un saut de ligne pour une meilleure lisibilité */
-    putchar('\n');
+	/* Ajout d'un saut de ligne pour une meilleure lisibilité */
+	putchar('\n');
 
-    return 0;
+	return (0);
 }
 
