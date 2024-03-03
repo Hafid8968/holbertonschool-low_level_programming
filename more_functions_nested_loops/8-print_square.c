9ac8c1ec9e9a00cfe5be8a5880472326f52ca201
Hafid8968 <8968@holbertonstudents.com>
2024-03-03 15:47:15 +0100

    Add function to print a square in the terminal

--- more_functions_nested_loops/8-print_square.c
+++ more_functions_nested_loops/8-print_square.c
@@ -8,13 +8,13 @@ void print_square(int size)
 {
 	if (size <= 0)
 	{
-	_putchar('\n');	
+	_putchar('\n');
 	}
 	else
 	{
 	int i, j;
 
-	for (i = 0; i < size; i++
+	for (i = 0; i < size; i++)
 	{
 	for (j = 0; j < size; j++)
 	{
