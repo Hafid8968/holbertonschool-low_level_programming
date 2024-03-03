b91508d4a3998a9490a450411438995588189098
Hafid8968 <8968@holbertonstudents.com>
2024-03-03 20:28:11 +0100

    Add function to print n elements of an integer array

--- pointers_arrays_strings/8-print_array.c
+++ pointers_arrays_strings/8-print_array.c
@@ -2,26 +2,26 @@
 #include <stdio.h>
 
 /**
- * print_array - Prints n elements of an array of integers, followed by a new line.
+ * print_array - Prints n elements of an integer array, followed by a new line.
  * @a: The array of integers.
  * @n: The number of elements to be printed.
  */
 void print_array(int *a, int n)
 {
-    int i;
+	int i;
 
-    for (i = 0; i < n; i++)
-    {
-        if (i == 0)
-        {
-            _putchar(a[i]);
-        }
-        else
-        {
-            _putchar(', ');
-            _putchar(a[i]);
-        }
-    }
-    _putchar('\n');
+	for (i = 0; i < n; i++)
+	{
+	if (i == 0)
+	{
+	printf(a[i]);
+	}
+	else
+	{
+	printf(', ');
+	printf(a[i]);
+	}
+	}
+	printf('\n');
 }
 
