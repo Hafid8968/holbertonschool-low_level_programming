e892fceb5f92e9f19bebe233042878e0443265d7
Hafid8968 <8968@holbertonstudents.com>
2024-03-03 22:30:53 +0100

    Add function to convert lowercase to uppercase in string with string_toupper

--- pointers_arrays_strings/5-string_toupper.c
+++ pointers_arrays_strings/5-string_toupper.c
@@ -2,7 +2,7 @@
 
 /**
  * string_toupper - Changes all lowercase letters of a string to uppercase.
- * @str: The input string.
+ * @s: The input string.
  *
  * Return: A pointer to the modified string.
  */
