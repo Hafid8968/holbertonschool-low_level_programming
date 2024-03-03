fd7bfa66bc8aed2ee80a4ad71011a0297aed7311
Hafid8968 <8968@holbertonstudents.com>
2024-03-03 21:57:37 +0100

    Add function to copy a string with limited bytes using _strncpy

--- pointers_arrays_strings/2-strncpy.c
+++ pointers_arrays_strings/2-strncpy.c
@@ -12,15 +12,18 @@ char *_strncpy(char *dest, char *src, int n)
 {
     int i;
 
-    for (i = 0; i < n && src[i] != '\0'; i++)
+    i = 0;
+
+    while (src[i] != '\0' && i < n)
     {
         dest[i] = src[i];
+        i++;
     }
 
-    // If n is greater than the length of src, pad dest with null bytes
-    for (; i < n; i++)
+    while (i < n)
     {
         dest[i] = '\0';
+        i++;
     }
 
     return dest;
