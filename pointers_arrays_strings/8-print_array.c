106ae179aba1e67b5fe128a328faccfa7a3fd465
Hafid8968 <8968@holbertonstudents.com>
2024-03-03 20:30:47 +0100

    Add function to print n elements of an integer array

--- pointers_arrays_strings/8-print_array.c
+++ pointers_arrays_strings/8-print_array.c
@@ -13,13 +13,12 @@ void print_array(int *a, int n)
 	for (i = 0; i < n; i++)
 	{
 	if (i == 0)
-	{
-	printf(a[i]);
+	}
+	printf(", %d", a[i]);
 	}
 	else
 	{
-	printf(', ');
-	printf(a[i]);
+	printf("%d", a[i]);
 	}
 	}
 	printf('\n');
