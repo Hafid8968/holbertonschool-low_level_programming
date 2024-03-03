e820e7a091177f36afacab7c6801796eba94e5b1
Hafid8968 <8968@holbertonstudents.com>
2024-03-03 21:55:16 +0100

    Add function to copy a string with limited bytes using _strncpy

---
+++ pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * _strncpy - Copies up to n characters from src to dest.
+ * @dest: The destination string.
+ * @src: The source string to be copied.
+ * @n: The maximum number of characters to copy from src.
+ *
+ * Return: A pointer to the resulting string dest.
+ */
+char *_strncpy(char *dest, char *src, int n)
+{
+    int i;
+
+    for (i = 0; i < n && src[i] != '\0'; i++)
+    {
+        dest[i] = src[i];
+    }
+
+    // If n is greater than the length of src, pad dest with null bytes
+    for (; i < n; i++)
+    {
+        dest[i] = '\0';
+    }
+
+    return dest;
+}
+
